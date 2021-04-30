73da06b9a5d37b1fd5a012187736cc116182dd13
AtifZawadBakshi <atif.zawad.bakshi@g.bracu.ac.bd>
2021-04-30 18:56:06 +0600

    first commit

---
+++ Models/users/layout/_footer.php
@@ -0,0 +1,9 @@
+<footer class="nav navbar-fixed-bottom">
+             <b class="text-danger">&COPY; CSE470 Project By Atif Zawad Bakshi</b>
+            <br>
+</footer>
+
+
+
+</body>
+</html>
---
+++ Views/LayoutGeneral/_footer.php
@@ -0,0 +1,9 @@
+<footer class="nav navbar-fixed-bottom">
+            <b class="text-danger">&COPY; CSE470 Project by Atif Zawad Bakshi</b>
+            <br>
+</footer>
+
+
+
+</body>
+</html>
